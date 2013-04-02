06bd3a79b402d6ffb29684069b28821f00aa0679
pedrohasselmann <hasselmann@on.br>
2013-04-02 16:17:33 -0300

    kernel.py
    Kernel containing the classification module.

---
+++ kernel.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Filename: central_method.py
+'''Neighboring Point Central Method'''
+
+# Escrevendo em python3 e usando python2.6:
+from __future__ import print_function, unicode_literals, absolute_import, division
+
+from gmode_module import stats, hyp_test, Invert, free
+from barycenter import barycenter, barycenter_hist, barycenter_KNN
+#from plot_module import plot_group, plot_distribution
+from file_module import l_to_s
+from itertools import imap
+from numpy import array, sqrt, median, diagonal, diagflat, eye, ndenumerate
+from numpy import any as aany
+from numpy import sum as asum
+from numpy import round as arround
+from plot_module import plot_clump
+
+#from Gmode_v3 import make_dir
+#import os
+#pathjoin = os.path.join
+  
+############ Neighboring Central Method: Recognition of Classes and Classification ###########
+ 
+def classifying(q1, vlim, minlim, grid, design, data, devt, report):
+
+    N = data.shape[0]    # Sample Number
+    M = data.shape[1]
+
+#_______________________________Whiten the sample__________________________
+
+    #elems = data
+    data = data/devt
+
+#________________________________Barycenter______________________________________
+
+    #from subroutines_gmode import barycenter_fast
+    #seed = barycenter_fast(50, design, data, devt)
+
+    seed = barycenter_hist(grid, design, data)
+
+    if len(seed) > 2:
+       report.append("Barycenter: "+l_to_s(map(lambda j: design[j], seed)))
+       #print("Barycenter: "+l_to_s(map(lambda j: design[j], seed)))
+       Na = len(seed)
+       group = list(seed)
+    else:      
+       return [], seed, report
+
+# ______________________________CLASSIFICATION__________________________________
+
+    i = 0
+    Rg, Na_prior  = 1e0, Na #eye(M), Na
+    
+    #make_dir(pathjoin("TESTS",label,"plots","Clump"+str(Nc),""))
+
+    #(round(asum(Rg),6) != round(asum(R_prior),6) and Na != Na_prior)
+    while (i == 0 or (round(Rg,6) != round(R_prior,6) and Na != Na_prior)) and i < 20 and Na > 2:
+
+          R_prior = Rg
+
+# *g --> group statistics
+
+          ctg, devg, Sg, Rg = stats(data[group])
+          #devg = vlim
+
+# Algorithm to replace zeroth deviations:
+
+          if i == 0 : #and Na == Na_prior and any(zeroth): 
+             for n, cov in ndenumerate(devg):
+              #if i == 0 :
+                 if cov < minlim[n]:
+                    devg[n] = minlim[n]
+                 
+              #if d > vlim[n]:
+              #   devg[n] = vlim[n]
+
+          Na_prior = Na
+
+          #plot_clump(i+1, [ctg*devt, devg*devt, Rg], elems[group], pathjoin(label,"plots","Clump"+str(Nc)), lim=[0.2,1.8], norm=[1,1e0], axis=[0.36,0.47,0.62,0.75,0.89])
+             
+
+# G hypothesis test:
+
+          #iSg, iRg = Invert(Sg), Invert(Rg)
+          #f = free(Rg)
+
+          f  = (M**2)/asum(Rg)
+          Rg = f/M
+
+          group = filter(lambda x: x != None, \
+                  imap(lambda ind, y: hyp_test(q1,f,ind,y,ctg,devg,Rg), range(N), data))
+
+          Na = len(group)
+
+          report.append("Run "+str(i)+" Size: "+str(Na)+" A.D.: "+l_to_s(arround(devg, 3))+"\nf: "+str(f)+"\n")
+
+          i += 1
+
+    return group, seed, report
+
+# End of the Central Method
