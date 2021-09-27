98e294a22a2b986ceccdb4e148ae5305ca1a8ce8
Pedro H. Hasselmann <pedrohasselmann@gmail.com>
2021-09-27 10:57:39 +0200

    Delete script.py

--- script.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import Gmode as Gm
-
-gmode = Gm.Gmode()
-gmode.load_data(filename="SDSSMOC/lists/MOC4_2quartile_refl.dat")
-gmode.run(q1=1.3,mlim=0.15,ulim=1.0,grid=2,save='y', name="MOC2q")
-gmode.evaluate(q2=1.3)
-gmode.correspondence("SDSSMOC/bus_templates.pkl", "bus",q1=2.0, var = range(1,gmode.elems.shape[1])) 
-
